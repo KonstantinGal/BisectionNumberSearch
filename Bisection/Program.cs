b2074c315e8a27870982bc384dc9baa74ba6767e
Konstantin Galkin <galkink@my.erau.edu>
2019-04-23 16:03:11 -0400

    Add project files.

---
+++ Bisection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bisection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
